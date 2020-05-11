abff2869051f3863691688c007b2913b023109fc
sebastianHan <736602840@qq.com>
2020-05-11 21:49:14 +0800

    fix CSocket

--- net/CSocket.cpp
+++ net/CSocket.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "CSocket.h"
+
+using namespace neco;
+using namespace neco::net;
+
+CSocket::CSocket(int nSockFd)
+    :m_nSockFd(m_nSockFd)
+{
+
+}
+
+CSocket::~CSocket()
+{
+
+}
+
+int CSocket::GetSocketFd() const
+{
+
+}
+
+void CSocket::BindAddredd(const CInetAddress& iLocalAddr)
+{
+
+}
+
+void CSocket::Listen()
+{
+
+}
+
+int CSocket::Accept(CInetAddress * iPeerAddr)
+{
+
+}
+
+void CSocket::SetReuseAddr(bool bOn)
+{
+
+}
